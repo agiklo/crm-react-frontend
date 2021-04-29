cfd476af202009d634d0a26bc28cd59e80337519
agiklo <54153910+agiklo@users.noreply.github.com>
2021-04-30 01:49:37 +0200

    feat: create Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer className="bg-white sticky-footer">
+                <div className="container my-auto">
+                    <div className="text-center my-auto copyright"><span>Copyright © CRM System 2021 | Made with ❤ by
+            <a href="https://github.com/agiklo"> Mateusz Milewczyk</a></span></div>
+                </div>
+            </footer>
+
+        )
+    }
+}
+
+export default Footer;
