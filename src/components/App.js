a1bb6358610af57a9030f6a03dbad14ac7478ea0
agiklo <54153910+agiklo@users.noreply.github.com>
2021-04-30 02:54:06 +0200

    feat: add images, sidebarlogin and new icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Login from "./Login";
 
+import Login from "./Login";
 import NavBar from './NavBar'
 import Products from "./Products";
+import Footer from "./Footer";
 
 class App extends React.Component {
+
     render() {
         return (
-            <Router>
+        <Router>
                 <div className={"App"}>
-                    <NavBar/>
                     <div className={"content"}>
+                        <NavBar/>
                         <Switch>
-                            <Route exact path={"/"}>
+                            <Route exact path={"/login"}>
                                 <Login/>
                             </Route>
                             <Route path={"/products"}>
                                 <Products/>
                             </Route>
                         </Switch>
+                        <Footer/>
                     </div>
                 </div>
             </Router>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 
 import App from './components/App'
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
