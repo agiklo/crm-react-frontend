3af20e536ea955d4d468d19b577993f9c77855b7
agiklo <54153910+agiklo@users.noreply.github.com>
2021-05-19 21:44:14 +0200

    refactor: move all users cards to one container, add exports buttons and add new employee

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import './Employees.css'
+import {Button} from "react-bootstrap";
 
 const api = axios.create({
     baseURL: `http://localhost:8080/api/v1/employees`
@@ -8,51 +9,139 @@ const api = axios.create({
 
 class Employees extends React.Component {
     state = {employees: []}
+    pageNumber = 0;
+    sizeNumber = 20;
 
     constructor() {
         super();
         this.getEmployees()
     }
 
+    setupPage = (pageParam) => {
+        this.pageNumber = pageParam
+        this.getEmployees()
+    }
+
+    setupSize = (sizeParam) => {
+        this.sizeNumber = sizeParam
+        this.getEmployees()
+    }
+
     getEmployees = async () => {
         try {
-            let data = await api.get('').then(({data}) => data);
+            let data = await api.get('',{
+                // headers: {
+                //     Authorization: 'Bearer ' + localStorage.getItem('token')
+                // },
+                params: {
+                    page: this.pageNumber,
+                    size: this.sizeNumber
+                }}).then(({data}) => data);
             this.setState({employees: data})
         } catch (err) {
-
             console.log(err)
         }
     }
 
+    exportToPdf = async () => {
+        await  api.get(`/export/pdf`, {
+            headers: {
+                accept: 'application/pdf'
+            }
+        })
+    }
+
+    exportToExcel = async () => {
+        await  api.get(`/export/excel`, {
+            headers: {
+                accept: 'application/vnd.ms-excel'
+            }
+        })
+    }
+
     render() {
         return (
-            <div className="container my-5">
-                <div className="row">
-                    {this.state.employees.map(employee =>
-                    <div className="col-md-3" key={employee.id}>
-                            <div className={"contact-box center-version"}>
-                                <a href="#profile.html">
-                                    <img alt="image" className="img-circle"
-                                         src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"/>
-                                    <h3 className="m-b-xs"><strong>{employee.firstName} {employee.lastName}</strong></h3>
-                                    <div className="font-bold"/>
-                                    <address className="m-t-md">
-                                        <strong>{employee.role}</strong><br/>
-                                        {employee.departmentName}<br/>
-                                    </address>
-
-                                </a>
-                                <div className="contact-box-footer">
-                                    <div className="m-t-xs btn-group">
-                                        <a className="btn btn-xs btn-white">Phone
-                                        <span></span>
-                                        </a>
-                                        <a className="btn btn-xs btn-white">Email</a>
-                                        <a className="btn btn-xs btn-white">Edit</a>
+            <div className={"container"}>
+                <div className="container-fluid m-2">
+                    <h3 className="text-dark mb-4">Employees</h3>
+                    <div className="card shadow">
+                        <div className="card-header py-3">
+                            <p className="text-primary m-0 font-weight-bold">Employees Info</p>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-md-6 text-nowrap">
+                                    <div id="dataTable_length" className="dataTables_length" aria-controls="dataTable">
+                                        <label>Show <select
+                                            className="form-control form-control-sm custom-select custom-select-sm">
+                                            <option onChange={() => this.setupSize(20)} value="20" value={"selected"}>20</option>
+                                            <option onClick={() => this.setupSize(30)} value="30">30</option>
+                                            <option onChange={() => this.setupSize(50)} value="50">50</option>
+                                            <option onChange={() => this.setupSize(100)} value="100">100</option>
+                                        </select></label>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="text-md-right dataTables_filter" id="dataTable_filter"><label>
+                                        <Button className={"mr-sm-2"}>Add new Employee</Button>
+                                        <Button className={"mr-sm-2"} onClick={() => this.exportToPdf()}>Export to PDF</Button>
+                                        <Button onClick={() => this.exportToExcel()}>Export to Excel</Button>
+                                    </label>
                                     </div>
                                 </div>
                             </div>
-                    </div>)}
+                            <div className="table-responsive table mt-2" id="dataTable" role="grid"
+                                 aria-describedby="dataTable_info">
+                                <div className="container my-5">
+                                    <div className="row">
+                                        {this.state.employees.map(employee =>
+                                            <div className="col-md-3" key={employee.id}>
+                                                <div className={"contact-box center-version"}>
+                                                    <a href="#profile.html">
+                                                        <img alt="image" className="img-circle"
+                                                             src="https://images.unsplash.com/photo-1558244661-d248897f7bc4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"/>
+                                                        <h3 className="m-b-xs"><strong>{employee.firstName} {employee.lastName}</strong></h3>
+                                                        <div className="font-bold"/>
+                                                        <address className="m-t-md">
+                                                            <strong>{employee.role}</strong><br/>
+                                                            {employee.departmentName}<br/>
+                                                        </address>
+
+                                                    </a>
+                                                    <div className="contact-box-footer">
+                                                        <div className="m-t-xs btn-group">
+                                                            <a className="btn btn-xs btn-white">Phone
+                                                                <span></span>
+                                                            </a>
+                                                            <a className="btn btn-xs btn-white">Email</a>
+                                                            <a className="btn btn-xs btn-white">Edit</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>)}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6 align-self-center">
+                                    <p id="dataTable_info" className="dataTables_info" role="status"
+                                       aria-live="polite">Showing {this.state.employees.length} to 20 of {this.state.employees.length}</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <nav
+                                        className="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                                        <ul className="pagination">
+                                            <li className="page-item disabled"><a className="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                                            <li className="page-item"><a className="page-link" onClick={() => this.setupPage(0)}>1</a></li>
+                                            <li className="page-item"><a className="page-link" onClick={() => this.setupPage(1)}>2</a></li>
+                                            <li className="page-item"><a className="page-link" onClick={() => this.setupPage(2)}>3</a></li>
+                                            <li className="page-item"><a className="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
